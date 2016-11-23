04ad70048e1fa75e87a3b075b843dd340491ce73
decoejz <decoejz@gmail.com>
2016-11-23 16:59:33 -0200

    salvando

--- usuario/setores/adicionar/dados_add.js
+++ usuario/setores/adicionar/dados_add.js
@@ -1,15 +1,24 @@
-function dados_adicionar() {
-    var x = document.getElementById("desc");
-    var text = "";
-    var i;
-    for (i = 0; i < x.length ;i++) {
-        text += x.elements[i].value + "<br>";
-    }
-    document.getElementById("demo").innerHTML = text;
-	window.history.back();
-}
+document.addEventListener("DOMContentLoaded", function() {
+	var dados = function dados_adicionar() {
+		    var x = document.getElementById("desc");
+		    var text = "";
+		    var i;
+		    for (i = 0; i < x.length ;i++) {
+		        text += x.elements[i].value + "<br>";
+		    }
+		    document.getElementById("demo").innerHTML = text;
+			window.history.back();
+		}
 
 
-function voltar() {
-	window.history.back();
+	var voltar_tela = function voltar() {
+			window.history.back();
+		}
+
+	var salvando = document.querySelector(".salvando");
+	var cancelando = document.querySelector(".cancelando");
+
+	salvando.addEventListener("click", dados);
+
+	cancelando.addEventListener("click", voltar_tela);
 }

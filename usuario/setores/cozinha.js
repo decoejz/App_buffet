cfc3e1514c2a7c6d30cc81d239e00b30744c2f17
decoejz <decoejz@gmail.com>
2016-11-26 22:39:05 -0200

    extras

--- usuario/setores/cozinha.js
+++ usuario/setores/cozinha.js
@@ -7,10 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
 		var infos = JSON.parse(conversao);
 		
 		for (var i in infos) {				
-			var ul = document.getElementById("lista_de_tarefas");
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode(i));
-			ul.appendChild(li);
+			var a = document.createElement("a");
+			var ulist = document.getElementById("lista_de_tarefas");
+			var newItem = document.createElement("li");
+
+			a.textContent = i;
+			a.setAttribute('href', "../../cadastro/pag_indisponivel.html");
+			newItem.appendChild(a);
+			ulist.appendChild(newItem);
 		}
 	}
 	else{
